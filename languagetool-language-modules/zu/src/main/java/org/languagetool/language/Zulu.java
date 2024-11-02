7ffaf30204a48b681361231859a702263946a70f
Brian Button <brianbuttonex@gmail.com>
2024-11-02 15:40:21 -0400

    Add Zulu

---
+++ languagetool-language-modules/zu/src/main/java/org/languagetool/language/Zulu.java
@@ -0,0 +1,98 @@
+package org.languagetool.language;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.languagetool.*;
+import org.languagetool.rules.*;
+import org.languagetool.rules.spelling.SpellingCheckRule;
+import org.languagetool.rules.spelling.hunspell.HunspellRule;
+import org.languagetool.tools.Tools;
+//import org.languagetool.tagging.Tagger;
+//import org.languagetool.tagging.disambiguation.Disambiguator;
+//import org.languagetool.tagging.disambiguation.rules.XmlRuleDisambiguator;
+//import org.languagetool.tagging.zu.ZuluTagger;
+//import org.languagetool.tokenizers.*;
+//import org.languagetool.tokenizers.zu.ZuluWordTokenizer;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Zulu extends Language {
+
+  /*@Override
+  public SentenceTokenizer createDefaultSentenceTokenizer() {
+    return new SRXSentenceTokenizer(this);
+  }*/
+
+  /*@Override
+  public Tokenizer createDefaultWordTokenizer() {
+    return new ZuluWordTokenizer();
+  }*/
+
+  @Override
+  public String getName() {
+    return "Zulu";
+  }
+  
+  @Override
+  public String getShortCode() {
+    return "zu";
+  }
+
+  @Override
+  public String[] getCountries() {
+    return new String[]{"ZA"};
+  }
+
+  @Override
+  public String getCommonWordsPath() {
+    // TODO: provide common words file
+    return null;
+  }
+
+  @Override
+  public Contributor[] getMaintainers() {
+    return new Contributor[] { Contributors.BRIAN_BUTTON };
+  }
+
+  /*@NotNull
+  @Override
+  public Tagger createDefaultTagger() {
+    return new ZuluTagger();
+  }
+
+  @Override
+  public Disambiguator createDefaultDisambiguator() {
+    return new XmlRuleDisambiguator(new Zulu());
+  }*/
+
+  @Override
+  public List<Rule> getRelevantRules(ResourceBundle messages, UserConfig userConfig, Language motherTongue, List<Language> altLanguages) {
+    return Arrays.asList(
+            new CommaWhitespaceRule(messages,
+              Example.wrong("Sibe nekhofi<marker> ,</marker> ushizi namakhukhi namagilebhisi."),
+              Example.fixed("Sibe nekhofi<marker>,</marker> ushizi namakhukhi namagilebhisi.")),
+            new DoublePunctuationRule(messages),
+            new GenericUnpairedBracketsRule(messages),
+            new HunspellRule(messages, this, userConfig, altLanguages),
+            new UppercaseSentenceStartRule(messages, this,
+              Example.wrong("Le ndlu indala. <marker>yakhiwa</marker> ngo-1950."),
+              Example.fixed("Le ndlu indala. <marker>Yakhiwa</marker> ngo-1950."),
+              Tools.getUrl("https://languagetool.org/insights/post/spelling-capital-letters/")),
+            new WordRepeatRule(messages, this),
+            new MultipleWhitespaceRule(messages, this),
+            new SentenceWhitespaceRule(messages)
+    );
+  }
+
+  @Override
+  public LanguageMaintainedState getMaintainedState() {
+    return LanguageMaintainedState.ActivelyMaintained;
+  }
+
+  @Nullable
+  @Override
+  protected SpellingCheckRule createDefaultSpellingRule(ResourceBundle messages) throws IOException {
+    return new HunspellRule(messages, this, null,null);
+  }
+}
---
+++ languagetool-language-modules/zu/src/main/resources/org/languagetool/resource/zu/hunspell/README_zu.txt
@@ -0,0 +1,12 @@
+Zulu Dictionary.
+
+Source: Hunspell dictionary from Zulu OpenOffice extension in (files zu_ZA.{aff,doc})
+in dict-zu_za-2010.01.26.oxt available https://extensions.openoffice.org/en/download/3565
+
+
+License of the file zu_ZA.{aff,dic} in dict-zu_za-2010.01.26.oxt:
+===
+
+
+Unknown or
+GNU Library or Lesser General Public License version 2.0 (LGPLv2), GNU General Public License version 2.0 (GPLv2), Mozilla Public License 1.1 (MPL 1.1)
